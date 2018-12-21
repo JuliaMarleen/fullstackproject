bf1485133f8293d498d0d5a79c5266428398d3cb
Marleen <aan_marleen@hotmail.com>
2018-12-21 14:15:45 +0100

    pagination export

--- pagination.js
+++ pagination.js
@@ -3,32 +3,25 @@
 //     return (((total - start) - limit) <= 0) ? (total - start) + 1 : (limit === 0) ? total : limit;
 // }
 
-function NumberOfPages(total, start, limit){
-    return Math.ceil(total / limit) //ceil naar boven afronden
+module.exports = { 
+    numberOfPages: function(total, start, limit){
+        return Math.ceil(total / limit) //ceil naar boven afronden
+    },
+    currentPage: function(total, start, limit) {
+        let totalPages =  Math.ceil(total / limit)
+        let pages = (total - start) / limit;
+        return Math.ceil(totalPages - pages)
+    },
+    getFirstQueryString: function(total, start, limit){
+        return `?start=${start}&limit=${limit}`
+    },
+    getLastQueryString: function(total, start, limit){
+        return `?start=${total}&limit=${limit}`
+    },
+    getPreviousQueryString: function(total, start, limit){
+        return `?start=${(total - limit > 0) ? (start - limit) : start}&limit=${limit}`
+    },
+    getNextQueryString: function(total,start,limit){
+        return `?start=${(total + limit > total) ? (start + limit) : start}&limit=${limit}`
+    }
 }
-
-function currentPage(total, start, limit){
-    let totalPages =  Math.ceil(total / limit)
-    let pages = (total - start) / limit;
-    return Math.ceil(totalPages - pages)
-}
-
-function getFirstQueryString(total, start, limit){
-    return `?start=${start}&limit=${limit}`
-}
-
-function getLastQueryString(total, start, limit){
-    return `?start=${total}&limit=${limit}`
-}
-
-function  getPreviousQueryString(total, start, limit){
-    return `?start=${(total - limit > 0) ? (start - limit) : start}&limit=${limit}`
-}
-
-function  getNextQueryString(total, start, limit){
-    return `?start=${(total + limit > total) ? (start + limit) : start}&limit=${limit}`
-}
-
-// function  getPagination(total, start, limit){
-
-// }
